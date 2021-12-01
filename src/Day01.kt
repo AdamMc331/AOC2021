cb2580a5d004a2a81de511a61018cb79dceb11a0
amcneilly <amcneilly331@gmail.com>
2021-12-01 15:51:48 -0500

    Completing Day01.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,36 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        // Convert to numbers
+        val inputNums = input.map { it.toInt() }
+
+        // Split into groups of two
+        val steps = inputNums.windowed(size = 2)
+
+        // If the second number is bigger than the first, we have an increase.
+        return steps.count { (first, second) ->
+            second > first
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        // Convert to numbers
+        val inputNums = input.map { it.toInt() }
+
+        // Split into groups of three, and get sums.
+        val sums = inputNums.windowed(size = 3).map { it.sum() }
+
+        // Window sums
+        val steps = sums.windowed(size = 2)
+
+        // If the second number is bigger than the first, we have an increase.
+        return steps.count { (first, second) ->
+            second > first
+        }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
 
     val input = readInput("Day01")
     println(part1(input))
